0222e393bd8d8e88f37740a9f40c937076caba3e
Damian Kolaska <damian.kolaska99@gmail.com>
2025-07-14 22:25:29 +0200

    feat(web): display file list

--- service/flux/src/main/java/com/gro4t/flux/controller/FileController.java
+++ service/flux/src/main/java/com/gro4t/flux/controller/FileController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/files")
+@CrossOrigin(origins = "http://localhost:5173")
 @AllArgsConstructor
 public class FileController {
     @Autowired
--- view/flux-web/src/App.tsx
+++ view/flux-web/src/App.tsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from 'react';
 import FluxAppBar from './components/FluxAppBar';
+import {List} from '@mui/material';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ListItemText from '@mui/material/ListItemText';
+import ImageIcon from '@mui/icons-material/Image';
 
-function App() {
+export default function App() {
+  const [files, setFiles] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/files")
+      .then((response) => response.json())
+      .then((data) => {
+        const names = data.map((item) => item.name);
+        setFiles(names);
+      })
+      .catch((error) => console.error("Error fetching files:", error));
+  }, []);
+
   return (
     <>
       <FluxAppBar />
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {files.map((file) => (
+          <ListItem key={file}>
+            <ListItemAvatar>
+              <Avatar>
+                <ImageIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={file} secondary="Jan 9, 2014" />
+          </ListItem>
+        ))}
+      </List>
     </>
-  )
+  );
 }
-
-export default App
